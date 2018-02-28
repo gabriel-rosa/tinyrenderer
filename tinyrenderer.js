ffcbecf7f1417e575bc0b22d53902b71d3227834
Gabriel Rosa e Silva <gabriel-rosa@users.noreply.github.com>
2018-02-28 13:31:24 -0300

    initial commit

---
+++ tinyrenderer.js
@@ -0,0 +1,16 @@
+var canvas, ctx;
+canvas = document.createElement('canvas');
+ctx = canvas.getContext('2d');
+
+
+var test_image = new TGA();
+
+
+test_image.open( "images/test.tga", function(data){
+	var imageData = ctx.createImageData(test_image.header.width, test_image.header.height);
+
+	canvas.width = test_image.header.width;
+	canvas.height = test_image.header.height;
+
+	ctx.putImageData(test_image.getImageData(imageData), 0, 0);
+});
