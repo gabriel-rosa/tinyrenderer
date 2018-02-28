eda03db0af2fb9aaea0111177c9f415bc18be7a3
Gabriel Rosa e Silva <gabriel-rosa@users.noreply.github.com>
2018-02-28 13:38:57 -0300

    Update tinyrenderer.js

--- tinyrenderer.js
+++ tinyrenderer.js
@@ -1,11 +1,9 @@
 var canvas, ctx;
 canvas = document.createElement('canvas');
 ctx = canvas.getContext('2d');
-
+document.body.appendChild(canvas);
 
 var test_image = new TGA();
-
-
 test_image.open( "images/african_head_diffuse.tga", function(data){
 	var imageData = ctx.createImageData(test_image.header.width, test_image.header.height);
 
