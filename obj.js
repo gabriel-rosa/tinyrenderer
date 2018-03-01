9bfeee24201e770c843747b1941e3c7dda03f527
Gabriel Rosa e Silva <gabriel-rosa@users.noreply.github.com>
2018-03-01 13:10:06 -0300

    Update obj.js

--- obj.js
+++ obj.js
@@ -36,7 +36,7 @@ Model.prototype.parse = function(raw_text) {
 		if (line.length != 4)
 			continue;
 		
-		for (int j = 1; j < 4; ++j)
+		for (var j = 1; j < 4; ++j)
 			line[j] = parseFloat(line[j]);
 
 		switch (line[0]) {
