79bce8046bdc6d202c11ab4d22c844e437617c41
Gabriel Rosa e Silva <gabriel-rosa@users.noreply.github.com>
2018-03-01 10:23:21 -0300

    Update tinyrenderer.js

--- tinyrenderer.js
+++ tinyrenderer.js
@@ -3,7 +3,64 @@ canvas = document.createElement('canvas');
 ctx = canvas.getContext('2d');
 document.body.appendChild(canvas);
 
-var test_image = new TGA();
+var Point = function(x, y) {
+	this.x = x;
+	this.y = y;
+};
+
+function clear_canvas(color) {
+	ctx.fillStyle = color;
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function render_pixel(p, color) {
+	ctx.fillStyle = color;
+	ctx.fillRect(p.x, p.y, 1, 1);
+}
+
+// Bresenham's line algorithm
+function render_line(p1, p2, color) {
+	var width = Math.abs(p2.x - p1.x);
+	var height = Math.abs(p2.y - p1.y);
+	
+	var steep = false;
+	if (height > width) 
+		// transpose
+		var tmp = new Point(p1.x, p2.x);
+		p1.x = p1.y;
+		p1.y = tmp.x;
+		p2.x = p2.y;
+		p2.y = tmp.x;				
+		steep = true;
+	}
+
+	if (p1.x > p2.x) {
+		var tmp = p1;
+		p1 = p2;
+		p2 = tmp;
+	}
+	
+	for (var x = 0; x < width; ++x) {
+		var t = x / width;
+		var y = Math.floor( p1.y*(1.0-t) + p2.y*t );
+		
+		if (steep)
+			render_pixel(new Point(y, p1.x+x, color); // de-transpose
+		else
+			render_pixel(new Point(p1.x+x, y, color);
+	}
+}
+
+canvas.width = 100;
+canvas.height = 100;
+
+clear_canvas('black');
+
+render_line(new Point(13, 20), new Point(80, 40), 'white'); 
+render_line(new Point(20, 13), new Point(40, 80), 'red'); 
+render_line(new Point(40, 80), new Point(40, 8013, 20), 'red'); 
+
+/*var test_image = new TGA();
 test_image.open( "images/african_head_diffuse.tga", function(data){
 	var imageData = ctx.createImageData(test_image.header.width, test_image.header.height);
 
@@ -11,4 +68,4 @@ test_image.open( "images/african_head_diffuse.tga", function(data){
 	canvas.height = test_image.header.height;
 
 	ctx.putImageData(test_image.getImageData(imageData), 0, 0);
-});
+});*/
